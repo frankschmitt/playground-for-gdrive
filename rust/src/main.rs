5ca0e8438d321d02161c6558a7d4aae4966387bd
Frank Schmitt <frankschmitt@gmx.de>
2025-05-04 15:12:25 +0200

    Rust: working auth with drive_v3

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,66 +1,56 @@
-use google_drive::Client;
-use google_drive::Files;
+use drive_v3::Credentials;
 
 use std::io::{BufRead, BufReader};
 use std::fmt;
 use std::fs::File;
 use std::str;
 
+fn init_gdrive_v3() -> Result<Credentials, drive_v3::Error>{
 
-// read whole file, and return it as a single string
-pub fn read_string(filename: String) -> String {
-    let reader = BufReader::new(File::open(filename).expect("Cannot open file"));
-    let mut result = String::new();
-    let mut val: String;
-    for line in reader.lines() {
-        val = line.unwrap();
-        result += &(val + "\n");
+
+    // This is the file downloaded in the Setup section
+    let client_secrets_path = "client_secret.json";
+    // let client_id = String::from("927638576198-47dem4qc3q9009obg2m1ri5nj2trfk4c.apps.googleusercontent.com");
+
+    // The OAuth scopes you need
+    let scopes: [&'static str; 2] = [
+        "https://www.googleapis.com/auth/drive.metadata.readonly",
+        "https://www.googleapis.com/auth/drive.file",
+    ];
+
+    let credentials = Credentials::from_client_secrets_file(&client_secrets_path, &scopes)?;
+
+    // TODO make this a global setting!
+    let credentials_storage_path = "credentials.json";
+    // we've got credentials - store them !
+    credentials.store(&credentials_storage_path);
+    return Ok(credentials);
+}
+
+
+fn refresh_credentials() -> Result<Credentials, drive_v3::Error> {
+    // The OAuth scopes you need
+    let scopes: [&'static str; 2] = [
+        "https://www.googleapis.com/auth/drive.metadata.readonly",
+        "https://www.googleapis.com/auth/drive.file",
+    ];
+
+
+    // TODO make this a global setting!
+    let credentials_storage_path = "credentials.json";
+    let mut stored_credentials = Credentials::from_file(&credentials_storage_path, &scopes)?;
+
+    // Refresh the credentials if they have expired
+    if !stored_credentials.are_valid() {
+        stored_credentials.refresh()?;
+
+        // Save them so we don't have to refresh them every time
+        stored_credentials.store(&credentials_storage_path)?;
     }
-
-    return result;
-}
-
-
-async fn initialize_gdrive() {
-    let mut google_drive = Client::new_from_env("", "");
-
-    // Get the URL to request consent from the user.
-    // You can optionally pass in scopes. If none are provided, then the
-    // resulting URL will not have any scopes.
-    //let user_consent_url = google_drive.user_consent_url(&["some-scope".to_string()]);
-
-    // In your redirect URL capture the code sent and our state.
-    // Send it along to the request for the token.
-    let code = "thing-from-redirect-url";
-    let state = "state-from-redirect-url";
-    //let mut access_token = google_drive.get_access_token(code, state).await.unwrap();
-
-    // You can additionally refresh the access token with the following.
-    // You must have a refresh token to be able to call this function.
-    //access_token = google_ drive.refresh_access_token().await.unwrap();
-}
-
-async fn list_gdrive() { 
-    let client_id = String::from("927638576198-47dem4qc3q9009obg2m1ri5nj2trfk4c.apps.googleusercontent.com");
-    let client_secret = read_string("client_secret.json".to_string());
-    let redirect_uri = "";
-    let token = "";
-    let refresh_token = "";
-    let _google_drive = Client::new(
-        //String::from("client-id"),
-        //String::from("client-secret"),
-        //String::from("redirect-uri"),
-        //String::from("token"),
-        //String::from("refresh-token")
-        client_id, 
-        client_secret,
-        redirect_uri,
-        token,
-        refresh_token
-   );
+    return Ok(stored_credentials);
 }
 
 fn main() {
     println!("Hello, world!");
-    list_gdrive();
+    init_gdrive_v3();
 }
